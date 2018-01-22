0be8106a0512eee383ae1fbca109d46b4e908866
bazyl <svintcov95@gmail.com>
2018-01-22 20:15:56 +0200

    shorted the redirector

--- src/main/java/command/CommandEnum.java
+++ src/main/java/command/CommandEnum.java
@@ -43,7 +43,7 @@ public enum CommandEnum {
       this.command = new SetLocaleCommand();
     }
   },
-  ADMINPAGE{
+  ADMINISTRATORPAGE {
     {
       this.command = new AdminPageCommand();
     }
--- src/main/java/command/Redirector.java
+++ src/main/java/command/Redirector.java
@@ -5,20 +5,14 @@ import entity.Role;
 import javax.servlet.http.HttpServletRequest;
 
 public class Redirector {
+
+  private static final String COMMAND_POSTFIX = "PAGE";
+
   public static String getMenu(final Role role, final HttpServletRequest request) {
 
-    if (role.equals(Role.ADMINISTRATOR)) {
-      CommandEnum commandEnum = CommandEnum.ADMINPAGE;
-      return commandEnum.getCurrentCommand().execute(request);
-
-    } else if (role.equals(Role.CUSTOMER)) {
-      CommandEnum commandEnum = CommandEnum.CUSTOMERPAGE;
-      return commandEnum.getCurrentCommand().execute(request);
-
-    } else if (role.equals(Role.MASTER)) {
-      CommandEnum commandEnum = CommandEnum.MASTERPAGE;
-      return commandEnum.getCurrentCommand().execute(request);
-
+    if (role != null) {
+      return CommandEnum.valueOf(role.toString().concat(COMMAND_POSTFIX)).getCurrentCommand()
+          .execute(request);
     } else {
       return ConfigurationManager.getProperty("path.page.index");
     }
